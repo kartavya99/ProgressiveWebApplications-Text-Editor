2e971cd1cb3b53277e10e4632652daf188eb7cec
kartavya <kartavyas@gmail.com>
2022-07-05 13:01:14 +1000

    updated install.js with PSA installing logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -19,7 +19,7 @@ export const putDb = async (content) => {
   const store = tx.objectStore("jate");
   const request = store.add({ jate: content });
   const result = await request;
-  console.log(result);
+  console.log("🚀 - data saved to the database", result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
